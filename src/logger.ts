4a826861a14fc5f9b11ba5fc200e36b5a467a8ca
wajeht <58354193+wajeht@users.noreply.github.com>
2023-08-28 22:56:04 -0500

    feat: log it to console and to file

--- src/logger.ts
+++ src/logger.ts
@@ -1,22 +1,25 @@
 import pino from 'pino';
 import path from 'path';
 
-const today: string = new Date().toISOString().split('T')[0];
+const today = new Date().toISOString().split('T')[0];
 
-const streams = [{ stream: pino.destination(path.join(__dirname, '..', 'logs', `${today}.log`)) }];
+const fileStream = { stream: pino.destination(path.join(__dirname, '..', 'logs', `${today}.log`)) };
+const consoleStream = { stream: process.stdout }; // Stream to log to console
+
+const streams = [fileStream, consoleStream];
 
 const logger = pino(
-	{
-		useOnlyCustomProps: true,
-		level: 'info',
-		formatters: {
-			level: (label: string) => {
-				return { level: label };
-			},
-		},
-		timestamp: pino.stdTimeFunctions.isoTime,
-	},
-	pino.multistream(streams),
+  {
+    useOnlyCustomProps: true,
+    level: 'info',
+    formatters: {
+      level: (label: string) => {
+        return { level: label };
+      },
+    },
+    timestamp: pino.stdTimeFunctions.isoTime,
+  },
+  pino.multistream(streams),
 );
 
 export default logger;
