2bc737eda1230cd55b664697352cd27dd0edb9dd
wajeht <58354193+wajeht@users.noreply.github.com>
2023-08-28 23:23:35 -0500

    feat: restructure files

--- src/containers.ts
+++ src/containers.ts
@@ -1,11 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+
+const ENV_JSON = path.resolve(path.join(process.cwd(), 'env.json'));
+
+if (!fs.existsSync(ENV_JSON)) {
+	console.error('env.json does not exist.');
+	process.exit(1);
+}
+
+let parsedEnvData = null;
+
+try {
+	const envData = fs.readFileSync(ENV_JSON, 'utf-8');
+	parsedEnvData = JSON.parse(envData);
+} catch (error) {
+	console.error('Error reading or parsing env.json:', error);
+	process.exit(1);
+}
+
 export interface Container {
 	name: string;
 	username: string;
 	database: string;
 }
 
-export const containers: Container[] = [
-	{ name: 'postgres', username: 'username', database: 'database' },
-	{ name: 'postgres', username: 'username', database: 'database' },
-	{ name: 'postgres', username: 'username', database: 'database' },
-];
+export const containers: Container[] = parsedEnvData.containers;
--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,7 @@ import { containers } from './containers';
 logger.info('Script started. Scheduling cron job...');
 
 // run every 3 hours
-cron.schedule('*/5 * * * * *', async () => {
-	// cron.schedule('0 */3 * * *', async () => {
+cron.schedule('0 */3 * * *', async () => {
 	logger.info('Cron job started at:', new Date().toLocaleString());
 
 	logger.info('Running backup script...');
