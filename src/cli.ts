836bd83c24c594c3cfd4855a0679fd6828512051
Jaw <58354193+wajeht@users.noreply.github.com>
2023-09-10 17:02:54 -0500

    chore: format

--- src/cli.ts
+++ src/cli.ts
@@ -19,7 +19,10 @@
 // 	logger.info('Backup script completed.');
 // });
 
-import { input } from '@inquirer/prompts';
+import {
+	input,
+	// confirm
+} from '@inquirer/prompts';
 import { Command } from 'commander';
 import { version } from '../package.json';
 import { list, start, remove } from './commands';
@@ -31,73 +34,73 @@ program.name('capdb')
   .description('database management cli for caprover')
   .version(version);
 
-// prettier-ignore
 program
-  .command('add')
-  .description('add containers database credentials to backup')
-  .option('-c, --container <string>', 'container name')
-  .option('-u, --username <string>', 'database username')
-  .option('-d, --database <string>', 'database name')
-  .action(async (cmd) => {
-    let { container, username, database } = cmd;
+	.command('add')
+	.description('add containers database credentials to backup')
+	.option('-c, --container <string>', 'container name')
+	.option('-u, --username <string>', 'database username')
+	.option('-d, --database <string>', 'database name')
+	.action(async (cmd) => {
+		let { container, username, database } = cmd;
 
-    let modify = undefined;
+		let modify = undefined;
 
-    let sure = false;
+		let sure = false;
 
-    if (!container || !username || !database) {
+		if (!container || !username || !database) {
+			while (sure === false) {
+				if (!container) {
+					container = await input({
+						message: 'Enter container name',
+						validate: (value) => value.length !== 0,
+					});
+				}
 
-      while (sure === false) {
+				if (!database) {
+					database = await input({
+						message: 'Enter database name',
+						validate: (value) => value.length !== 0,
+					});
+				}
 
-        if (!container) {
-          container = await input({
-            message: 'Enter container name',
-            validate: (value) => value.length !== 0,
-          });
-        }
+				if (!username) {
+					username = await input({
+						message: 'Enter database username',
+						validate: (value) => value.length !== 0,
+					});
+				}
 
-        if (!database) {
-          database = await input({
-            message: 'Enter database name',
-            validate: (value) => value.length !== 0,
-          });
+				console.table({ container, username, database });
 
-        }
+				// sure = await confirm({ message: 'are you sure?' })
 
-        if (!username) {
-          username = await input({
-            message: 'Enter database username',
-            validate: (value) => value.length !== 0,
-          });
-        }
+				sure =
+					(await input({
+						message: 'Are you sure these are the correct information? (y/n)',
+						validate: (value) => value === 'y' || value === 'n',
+					})) === 'y'
+						? true
+						: false;
 
-        console.table({ container, username, database });
+				if (!sure) {
+					modify = await input({
+						message: 'what do you want to chagne? database (u), container (c), username (u) ?',
+						validate: (value) => ['c', 'u', 'd'].includes(value) === true,
+					});
 
-        // sure = await confirm({ message: 'are you sure?' })
+					container = modify === 'c' ? '' : container;
+					username = modify === 'u' ? '' : username;
+					database = modify === 'd' ? '' : database;
+				}
+			}
+		}
 
-        sure = await input({
-          message: 'Are you sure these are the correct information? (y/n)',
-          validate: (value) => value === 'y' || value === 'n',
-        }) === 'y' ? true : false;
-
-        if (!sure) {
-          modify = await input({
-            message: 'what do you want to chagne? database (u), container (c), username (u) ?',
-            validate: (value) => ['c', 'u', 'd'].includes(value) === true,
-          })
-
-          container = modify === 'c' ? '' : container
-          username = modify === 'u' ? '' : username
-          database = modify === 'd' ? '' : database
-        }
-
-      }
-    }
-
-    console.log('Adding database credentials to backup...', {
-      container, username, database
-    });
-  });
+		console.log('Adding database credentials to backup...', {
+			container,
+			username,
+			database,
+		});
+	});
 
 // prettier-ignore
 program
