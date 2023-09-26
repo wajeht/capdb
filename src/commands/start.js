7887f68d1f9cfbc6dd14921ea3b7d7eeca496a3d
wajeht <58354193+wajeht@users.noreply.github.com>
2023-09-25 23:48:10 -0500

    feat: backup working mvp

--- src/commands/start.js
+++ src/commands/start.js
@@ -1,29 +1,67 @@
 import { logger } from '../utils/logger.js';
 import { shell } from '../utils/shell.js';
 import { Database as db } from '../utils/database.js';
+
 import cron from 'node-cron';
+import path from 'path';
+import fs from 'fs';
+
+function ensureDirectoryExists(directory) {
+	if (!fs.existsSync(directory)) {
+		fs.mkdirSync(directory, { recursive: true });
+	}
+}
 
 async function backupDatabase(container) {
+	let fileName;
+	const backupDirectory = path.join(process.env.HOME, '.config', 'capdb', 'backups');
+
+	ensureDirectoryExists(backupDirectory);
+
 	switch (container.database_type) {
 		case 'postgres':
 			process.env.PGPASSWORD = container.database_password;
+			fileName = path.join(
+				backupDirectory,
+				`dump-${container.container_name}-${container.database_name}-${new Date()
+					.toISOString()
+					.replace(/:/g, '-')}.sql`,
+			);
 			await shell(
-				`docker exec -i ${container.container_name} pg_dump -U ${container.database_username} -d ${container.database_name} > dump.sql`,
+				`docker exec -i ${container.container_name} pg_dump -U ${container.database_username} -d ${container.database_name} > ${fileName}`,
 			);
 			delete process.env.PGPASSWORD;
 			break;
 		case 'mysql':
+			fileName = path.join(
+				backupDirectory,
+				`dump-${container.container_name}-${container.database_name}-${new Date()
+					.toISOString()
+					.replace(/:/g, '-')}.sql`,
+			);
 			await shell(
-				`docker exec -i ${container.container_name} mysqldump -u${container.database_username} -p${container.database_password} ${container.database_name} > dump.sql`,
+				`docker exec -i ${container.container_name} mysqldump -u${container.database_username} -p${container.database_password} ${container.database_name} > ${fileName}`,
 			);
 			break;
 		case 'mongodb':
+			fileName = path.join(
+				backupDirectory,
+				`dump-${container.container_name}-${container.database_name}-${new Date()
+					.toISOString()
+					.replace(/:/g, '-')}`,
+			);
 			await shell(
-				`docker exec -i ${container.container_name} mongodump --username ${container.database_username} --password ${container.database_password} --db ${container.database_name} --out /backup`,
+				`docker exec -i ${container.container_name} mongodump --username ${container.database_username} --password ${container.database_password} --db ${container.database_name} --out ${fileName}`,
 			);
 			break;
 		case 'redis':
-			await shell(`docker exec -i ${container.container_name} redis-cli save`);
+			fileName = path.join(
+				backupDirectory,
+				`dump-${container.container_name}-${container.database_name}-${new Date()
+					.toISOString()
+					.replace(/:/g, '-')}`,
+			);
+			await shell(`docker exec -i ${container.container_name} redis-cli save > ${fileName}`);
 			break;
 		default:
 			console.log(`Unsupported database type: ${container.database_type}`);
@@ -33,7 +71,7 @@ async function backupDatabase(container) {
 
 (async function start() {
 	const containers = db.getAll();
-	containers.map((container) => {
+	for (const container of containers) {
 		const task = cron.schedule(
 			`*/${container.back_up_frequency} * * * *`,
 			async () => {
@@ -47,6 +85,5 @@ async function backupDatabase(container) {
 		);
 		task.start();
 		logger(`backup scheduled for ${container.container_name}`);
-	});
-	process.exit(0);
+	}
 })();
