5a91de0799edde6f620dd52f6e05d5df3886898c
Jaw <58354193+wajeht@users.noreply.github.com>
2023-09-29 18:13:16 -0500

    fix: language

--- src/utils/database.js
+++ src/utils/database.js
@@ -66,7 +66,7 @@ export class Database {
 		const foundIndex = this.data.findIndex((container) => container.id === id);
 
 		if (foundIndex === -1) {
-			return console.error('\nNot found\n');
+			return console.error('\nThe specified container ID was not found.\n');
 		}
 
 		this.data.splice(foundIndex, 1);
@@ -87,7 +87,7 @@ export class Database {
 		const foundIndex = this.data.findIndex((c) => c.id === id);
 
 		if (foundIndex === -1) {
-			return console.log('Not found');
+			return console.error('\nThe specified container ID was not found.\n');
 		}
 
 		const updatedContainer = {
