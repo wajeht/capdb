dc334aa2fa956ce09ac2e81996ff939b6d38579d
Jaw <58354193+wajeht@users.noreply.github.com>
2023-10-15 16:03:35 -0500

    chore: release v1.10.0

--- src/commands/importt.js
+++ src/commands/importt.js
@@ -58,11 +58,13 @@ export async function importt(cmd) {
 	}
 
 	const containerInsertPromise = jsonData.containers.map((container) => {
+		// @eslint-ignore
 		const { id, ...rest } = container;
 		return db('containers').insert({ ...rest });
 	});
 
 	const configInsertPromise = jsonData.config.map((config) => {
+		// @eslint-ignore
 		const { id, ...rest } = config;
 		return db('configurations').insert({ ...rest });
 	});
