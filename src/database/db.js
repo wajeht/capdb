41035991f134b711af257966b71db55377d9cf89
wajeht <58354193+wajeht@users.noreply.github.com>
2023-10-14 23:24:07 -0500

    feat: add post install script

--- src/database/db.js
+++ src/database/db.js
@@ -1,16 +1,4 @@
 import knex from 'knex';
 import knexConfig from './knexfile.js';
 
-process.chdir('src/database');
-
-// knex(knexConfig)
-// 	.raw('SELECT 1 + 1')
-// 	.then(() => {
-// 		console.debug('Database connection started!');
-// 	})
-// 	.catch(() => {
-// 		console.error('Database connection failed!');
-// 		process.exit(1);
-// 	});
-
 export default knex(knexConfig);
