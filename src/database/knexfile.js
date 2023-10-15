8dc2949d8095846928d59f253718b6e3434b2fac
wajeht <58354193+wajeht@users.noreply.github.com>
2023-10-15 00:35:08 -0500

    fix(database): kenxfile was not recognizse

--- src/database/knexfile.js
+++ src/database/knexfile.js
@@ -1,8 +1,14 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const knexConfig = {
 	client: 'sqlite3',
 	useNullAsDefault: true,
 	connection: {
-		filename: './db.sqlite',
+		filename: path.resolve(__dirname, 'db.sqlite'),
 	},
 	migrations: {
 		tableName: 'knex_migrations',
