81a77de70e1f06b74f73733a6db23dc89fb7b02c
wajeht <58354193+wajeht@users.noreply.github.com>
2023-10-21 18:41:54 -0500

    fix: restart dely

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -18,7 +18,7 @@ PARENT_DIR="$(dirname "$DIR")"
 # 30 days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
 KILL_TIMEOUT_MS=$((30 * 24 * 60 * 60 * 1000))
 
-pm2 start $PARENT_DIR/commands/start.js --name capdb --kill-timeout $KILL_TIMEOUT_MS --restart-delay 5000
+pm2 start $PARENT_DIR/commands/start.js --name capdb --kill-timeout $KILL_TIMEOUT_MS --restart-delay 5000 --exp-backoff-restart-delay=100
 pm2 startup
 pm2 save --force
 
