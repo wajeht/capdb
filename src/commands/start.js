b9003dae6aeaa70a3aa8bfd91ba4dc4206a53310
wajeht <58354193+wajeht@users.noreply.github.com>
2023-10-21 19:07:03 -0500

    fix: cron issues

--- src/commands/start.js
+++ src/commands/start.js
@@ -1,12 +1,14 @@
-import Docker from 'dockerode';
+import { fork } from 'child_process';
 import { logger } from '../utils/logger.js';
 import { timeToCron } from '../utils/time-to-cron.js';
-import { shell } from '../utils/shell.js';
 import db from '../database/db.js';
 import { ensureDirectoryExists } from '../utils/utils.js';
 import cron from 'node-cron';
 import path from 'path';
 import fastq from 'fastq';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config = await db.select('*').from('configurations').first();
 
@@ -19,137 +21,33 @@ const backupDirectory = path.join(config.capdb_config_folder_path, 'backups');
 
 ensureDirectoryExists(backupDirectory);
 
-const docker = new Docker();
-
-const queue = fastq.promise(async (containerId) => {
-	ensureDirectoryExists(backupDirectory);
-	await performBackup(containerId);
-}, 1);
-
-async function performBackup(containerId) {
-	try {
-		logger(`Starting backup job for container ID: ${containerId}`);
-
-		const currentDate = new Date().toLocaleString();
-		const filePath = await backupDatabase(containerId);
-
-		if (filePath) {
-			const absoluteBackupFilePath = path.join(backupDirectory, filePath);
-			await updateContainerStatus(containerId, true, currentDate, absoluteBackupFilePath);
-			logger(`Successfully backed up container ID: ${containerId}`);
-		} else {
-			await updateContainerStatus(containerId, false, currentDate, null);
-			logger(`Backup failed for container ID: ${containerId}`);
-		}
-	} catch (error) {
-		logger(`Error in backup job for container ID: ${containerId}, ${error.message}`);
-	}
-}
+const queue = fastq.promise(handleBackup, 1);
 
 async function handleBackup(containerId) {
-	try {
-		const container = await db.select('*').from('containers').where({ id: containerId }).first();
-		if (container) {
-			queue.push(container.id, (err) => {
-				if (err) {
-					logger(`Error while processing backup for ${container.container_name}: ${err.message}`);
-				} else {
-					logger(`Backup job completed for ${container.container_name}`);
-				}
-			});
-		} else {
-			logger(`Container with ID ${containerId} not found`);
-		}
-	} catch (err) {
-		logger(`Error fetching container from database: ${err.message}`);
-	}
-}
-
-async function backupDatabase(containerId) {
 	ensureDirectoryExists(backupDirectory);
-
-	let fileName = '';
-	const currentDateISOString = new Date().toISOString().replace(/:/g, '-');
-
-	try {
-		logger(`Fetching container info from database`);
-		const container = await db.select('*').from('containers').where({ id: containerId }).first();
-		const dockerContainers = await docker.listContainers({ all: true });
-
-		const containerExists = dockerContainers.some((c) =>
-			c.Names.includes(`/${container.container_name}`),
-		);
-
-		if (!containerExists) {
-			logger(`Container ${container.container_name} does not exist.`);
-			return null;
-		}
-
-		logger(`Starting database backup for ${container.container_name}`);
-		switch (container.database_type) {
-			case 'postgres':
-				process.env.PGPASSWORD = container.database_password;
-				fileName = `dump-${container.container_name}-${container.database_name}-${currentDateISOString}.sql`;
-				// prettier-ignore
-				await shell(`docker exec -i ${container.container_name} pg_dump -U ${ container.database_username } -d ${container.database_name} > ${path.join(backupDirectory, fileName)}`);
-				delete process.env.PGPASSWORD;
-				break;
-
-			case 'mongodb':
-				fileName = `dump-${container.container_name}-${container.database_name}-${currentDateISOString}`;
-				// prettier-ignore
-				await shell(`docker exec -i ${container.container_name} mongodump --username ${ container.database_username } --password ${container.database_password} --db ${ container.database_name } --out ${path.join(backupDirectory, fileName)}`);
-				break;
-
-			default:
-				logger(`Unsupported database type: ${container.database_type}`);
-				return null;
-		}
-
-		return fileName;
-	} catch (error) {
-		logger(`Error during backup: ${error?.message}`);
-		return null;
-	}
-}
-
-async function updateContainerStatus(containerId, status, lastBackedUpAt, lastBackedUpFile) {
-	ensureDirectoryExists(backupDirectory);
-	try {
-		logger(`Updating container status in database`);
-		const updatedContainer = await db('containers').where('id', containerId).update({
-			status: status,
-			last_backed_up_at: lastBackedUpAt,
-			last_backed_up_file: lastBackedUpFile,
-		});
-
-		if (updatedContainer === 1) {
-			return true;
-		} else {
-			logger(`Failed to update container with ID ${containerId}`);
-			return false;
-		}
-	} catch (error) {
-		logger(`Error updating container with ID ${containerId}: ${error.message}`);
-		return false;
-	}
+	const backupWorker = fork(path.resolve(__dirname, '../utils/backup-worker.js'));
+	backupWorker.send({ containerId, backupDirectory });
+	backupWorker.on('message', (message) => {
+		logger(message);
+	});
 }
 
 async function start() {
 	ensureDirectoryExists(backupDirectory);
-
 	const containers = await db.select('*').from('containers');
-
 	if (!containers.length) {
-		logger(`No containers to back up.`);
+		logger('No containers to back up.');
 		process.exit(0);
 	}
-
 	containers.forEach((container) => {
 		const cronExpression = timeToCron(container.back_up_frequency);
-		logger(`Scheduling backup for ${container.container_name}`);
-		cron.schedule(cronExpression, async () => await handleBackup(container.id));
-		logger(`Backup scheduled for ${container.container_name}`);
+		cron.schedule(cronExpression, () => {
+			queue.push(container.id, (err) => {
+				if (err) {
+					logger(`Error while processing backup for ${container.container_name}: ${err.message}`);
+				}
+			});
+		});
 	});
 }
 
---
+++ src/utils/backup-worker.js
@@ -0,0 +1,108 @@
+import db from '../database/db.js';
+import { logger } from '../utils/logger.js';
+import { shell } from '../utils/shell.js';
+import path from 'path';
+import Docker from 'dockerode';
+import { ensureDirectoryExists } from '../utils/utils.js';
+
+const docker = new Docker();
+
+const config = await db.select('*').from('configurations').first();
+
+if (!config) {
+  logger('No configurations found in database. Please run `capdb config` first.');
+  process.exit(1);
+}
+
+const backupDirectory = path.join(config.capdb_config_folder_path, 'backups');
+
+ensureDirectoryExists(backupDirectory);
+
+process.on('message', async ({ containerId, backupDirectory }) => {
+  ensureDirectoryExists(backupDirectory);
+  try {
+    const currentDate = new Date().toLocaleString();
+    const filePath = await backupDatabase(containerId, backupDirectory);
+
+    if (filePath) {
+      const absoluteBackupFilePath = path.join(backupDirectory, filePath);
+      await updateContainerStatus(containerId, true, currentDate, absoluteBackupFilePath);
+      logger(`Successfully backed up container ID: ${containerId}`);
+    } else {
+      await updateContainerStatus(containerId, false, currentDate, null);
+      logger(`Backup failed for container ID: ${containerId}`);
+    }
+  } catch (error) {
+    logger(`Error in backup job for container ID: ${containerId}, ${error.message}`);
+  }
+});
+
+async function backupDatabase(containerId) {
+  ensureDirectoryExists(backupDirectory);
+
+  let fileName = '';
+  const currentDateISOString = new Date().toISOString().replace(/:/g, '-');
+
+  try {
+    logger(`Fetching container info from database`);
+    const container = await db.select('*').from('containers').where({ id: containerId }).first();
+    const dockerContainers = await docker.listContainers({ all: true });
+
+    const containerExists = dockerContainers.some((c) =>
+      c.Names.includes(`/${container.container_name}`),
+    );
+
+    if (!containerExists) {
+      logger(`Container ${container.container_name} does not exist.`);
+      return null;
+    }
+
+    logger(`Starting database backup for ${container.container_name}`);
+    switch (container.database_type) {
+      case 'postgres':
+        process.env.PGPASSWORD = container.database_password;
+        fileName = `dump-${container.container_name}-${container.database_name}-${currentDateISOString}.sql`;
+        // prettier-ignore
+        await shell(`docker exec -i ${container.container_name} pg_dump -U ${container.database_username} -d ${container.database_name} > ${path.join(backupDirectory, fileName)}`);
+        delete process.env.PGPASSWORD;
+        break;
+
+      case 'mongodb':
+        fileName = `dump-${container.container_name}-${container.database_name}-${currentDateISOString}`;
+        // prettier-ignore
+        await shell(`docker exec -i ${container.container_name} mongodump --username ${container.database_username} --password ${container.database_password} --db ${container.database_name} --out ${path.join(backupDirectory, fileName)}`);
+        break;
+
+      default:
+        logger(`Unsupported database type: ${container.database_type}`);
+        return null;
+    }
+
+    return fileName;
+  } catch (error) {
+    logger(`Error during backup: ${error?.message}`);
+    return null;
+  }
+}
+
+async function updateContainerStatus(containerId, status, lastBackedUpAt, lastBackedUpFile) {
+  ensureDirectoryExists(backupDirectory);
+  try {
+    logger(`Updating container status in database`);
+    const updatedContainer = await db('containers').where('id', containerId).update({
+      status: status,
+      last_backed_up_at: lastBackedUpAt,
+      last_backed_up_file: lastBackedUpFile,
+    });
+
+    if (updatedContainer === 1) {
+      return true;
+    } else {
+      logger(`Failed to update container with ID ${containerId}`);
+      return false;
+    }
+  } catch (error) {
+    logger(`Error updating container with ID ${containerId}: ${error.message}`);
+    return false;
+  }
+}
