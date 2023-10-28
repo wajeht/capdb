bdfcd0b5366d663e8d823a30e261592950a80793
Jaw <58354193+wajeht@users.noreply.github.com>
2023-10-28 04:26:19 -0500

    feat: only watch inside parent dir

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -18,7 +18,7 @@ PARENT_DIR="$(dirname "$DIR")"
 # 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
 KILL_TIMEOUT_MS=$((24 * 60 * 60 * 1000))
 
-pm2 start $PARENT_DIR/commands/start.js --name capdb --kill-timeout $KILL_TIMEOUT_MS --restart-delay 5000 --exp-backoff-restart-delay=100 --max-restarts=10000 --watch --ignore-watch="*.log"
+pm2 start $PARENT_DIR/commands/start.js --name capdb --kill-timeout $KILL_TIMEOUT_MS --restart-delay 5000 --exp-backoff-restart-delay=100 --max-restarts=10000 --watch $PARENT_DIR --ignore-watch="*.log"
 pm2 startup
 pm2 save --force
 
