f4963f0d4cd160e42ba47ddb0571198db1f57ae7
wajeht <58354193+wajeht@users.noreply.github.com>
2023-10-29 15:18:41 -0500

    chroe(remove): more remove tests

--- src/commands/remove.test.js
+++ src/commands/remove.test.js
@@ -1,12 +1,17 @@
+import db from '../database/db.js';
 import { remove } from './remove.js';
 import { describe, it, expect, vi, beforeAll, afterAll } from 'vitest';
-import db from '../database/db.js';
+import { input } from '@inquirer/prompts';
 
 describe('remove', () => {
 	beforeAll(async () => {
 		vi.spyOn(console, 'log').mockImplementation(() => {});
 		vi.spyOn(console, 'error').mockImplementation(() => {});
 		vi.spyOn(process, 'exit').mockImplementation(() => {});
+		vi.mock('@inquirer/prompts', () => ({
+			input: vi.fn(),
+		}));
+
 		await db.migrate.latest();
 	});
 
@@ -34,6 +39,51 @@ describe('remove', () => {
 			expect(console.error).toHaveBeenCalledWith('No containers found in the database.');
 			expect(process.exit).toHaveBeenCalledWith(1);
 			expect(console.log).toHaveBeenCalledTimes(3);
+		});
+	});
+
+	describe('when providing the --all flag', () => {
+		describe('when the user does confirm the information', () => {
+			it('should exist with Everything has been removed!', async () => {
+				// prettier-ignore
+				await db('containers').insert({ container_name: 'container_name', database_type: 'container_path', database_name: 'container_size', database_username: 'container_last_modified', database_password: 'container_last_modified', });
+				// prettier-ignore
+				await db('configurations').insert({ capdb_config_folder_path: 'path', s3_access_key: 'access_key', s3_secret_key: 'secret_key', s3_bucket_name: 'bucket_name', s3_region: 'region', });
+				input.mockImplementationOnce(() => 'y');
+				await remove({ all: true });
+
+				expect(input).toHaveBeenCalledWith(
+					expect.objectContaining({
+						message: 'Are you sure these above the correct information? (y/n)',
+					}),
+				);
+				expect(console.log).toHaveBeenCalledWith('Everything has been removed!');
+				expect(console.log).toHaveBeenCalledTimes(12);
+				expect(process.exit).toHaveBeenCalledWith(0);
+			});
+		});
+	});
+
+	describe('when providing the --all flag', () => {
+		describe('when the user does not confirm the information', () => {
+			it('should exit with Everything has been removed!x', async () => {
+				// prettier-ignore
+				await db('containers').insert({ container_name: 'container_name', database_type: 'container_path', database_name: 'container_size', database_username: 'container_last_modified', database_password: 'container_last_modified', });
+				// prettier-ignore
+				await db('configurations').insert({ capdb_config_folder_path: 'path', s3_access_key: 'access_key', s3_secret_key: 'secret_key', s3_bucket_name: 'bucket_name', s3_region: 'region', });
+
+				input.mockImplementationOnce(() => 'n');
+
+				await remove({ all: true });
+
+				expect(input).toHaveBeenCalledWith(
+					expect.objectContaining({
+						message: 'Are you sure these above the correct information? (y/n)',
+					}),
+				);
+				expect(console.log).toHaveBeenCalledWith('Ok, exited remove operation!');
+				expect(process.exit).toHaveBeenCalledWith(0);
+			});
 		});
 	});
 });
